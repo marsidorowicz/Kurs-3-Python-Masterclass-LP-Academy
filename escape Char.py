904cc8574a78d16ce02e9dfa4b838c0f04a592f4
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-04-04 15:14:39 +0200

    Operators

--- escape Char.py
+++ escape Char.py
@@ -4,3 +4,17 @@ print(splitString)
 tabbedString = "1\t2\t3\t4\t5"
 print(tabbedString)
 print('The same "bla" \'dziecko\'')
+
+print("C:\\Users\Mario\notes.txt")
+print("C:\\Users\Mario\\notes.txt")
+print(r"C:\Users\Mario\notes.txt")
+name = "Mariusz"
+age = 35
+print(age)
+
+print(type(age))
+
+age_words = "35"
+print(age)
+print(type(age))
+print(name + " is " + str(age) + " years old")
