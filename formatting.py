66c1d15f757f0df75efad26326656ffd0232f607
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-04-09 16:19:46 +0200

    Formatting

---
+++ formatting.py
@@ -0,0 +1,20 @@
+for i in range(1, 13):
+    print("Wynik dla i = {0:2} i do kwadratu = {1:3} a i do potęgi trzeciej = {2:4}".format(i, i**2, i**3))
+
+print()
+
+for i in range(1, 13):
+    print("Wynik dla i = {0:2} i do kwadratu = {1:<3} a i do potęgi trzeciej = {2:<4}".format(i, i**2, i**3))
+
+print()
+
+print("Pi wynosi w przybliżeniu {0:12}".format(22/7))
+print("Pi wynosi w przybliżeniu {0:12f}".format(22/7))
+print("Pi wynosi w przybliżeniu {0:12.50f}".format(22/7))
+print("Pi wynosi w przybliżeniu {0:52.50f}".format(22/7))
+print("Pi wynosi w przybliżeniu {0:62.50f}".format(22/7))
+print("Pi wynosi w przybliżeniu {0:<72.50f}".format(22/7))
+print("Pi wynosi w przybliżeniu {0:<72.54f}".format(22/7))
+
+for i in range(1, 13):
+    print("Wynik {} i do kwadratu = {} a i do potęgi trzeciej = {:<4}".format(i, i**2, i**3))
---
+++ sequence op.py
@@ -0,0 +1,6 @@
+string1 = "Abs "
+string2 = "my "
+string3 = "baby"
+
+print(string1+string2+string3)
+print(string1*5)
