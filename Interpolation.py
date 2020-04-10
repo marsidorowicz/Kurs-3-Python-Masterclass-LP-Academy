d500a58acbfc8d1b4ce6b4e40b0d6367e6096f8b
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-04-10 18:40:24 +0200

    Interpolation

---
+++ Interpolation.py
@@ -0,0 +1,6 @@
+age = 35
+print("Mam %d" % age)
+
+major = "lat"
+minor = "Miesiące"
+print("Mam %d %s i 3 %s" %(age, major, minor))
--- formatting.py
+++ formatting.py
@@ -18,3 +18,7 @@ print("Pi wynosi w przybliżeniu {0:<72.54f}".format(22/7))
 
 for i in range(1, 13):
     print("Wynik {} i do kwadratu = {} a i do potęgi trzeciej = {:<4}".format(i, i**2, i**3))
+
+print(f"Pi wynosi w przybliżeniu {22 / 7:12.12f}")
+pi = 22/7
+print(f"Pi wynosi w przybliżeniu {pi:12.20f}")
