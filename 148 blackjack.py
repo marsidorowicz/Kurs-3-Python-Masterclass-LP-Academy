e2a9134c8187e6566084a706a3fccf9f053c030b
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-05-04 18:35:35 +0200

    150 finished

--- 148 blackjack.py
+++ 148 blackjack.py
@@ -9,7 +9,7 @@ mainWindow.geometry("640x480")
 
 
 def load_images(card_images):
-    suits = ["heart", "clubs", "diamond", "spade"]
+    suits = ["heart", "club", "diamond", "spade"]
     face_cards = ["jack", "queen", "king"]
 
     if tkinter.TkVersion >= 8.6:
@@ -21,15 +21,32 @@ def load_images(card_images):
     for suit in suits:
         # numbered cards
         for card in range(1, 11):
-            name = "E:/programowanie/cards/{}_{}".format(str(card), suit, extension)
-            image = tkinter.PhotoImage(tile=name)
+            name = "E:\\Programowanie\\cards\\{}_{}.{}".format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
             card_images.append((card, image,))
 
         # face cards
         for card in face_cards:
-            name = "E:/programowanie/cards/{}_{}".format(str(card), suit, extension)
-            image = tkinter.PhotoImage(tile=name)
+            name = "E:\\Programowanie\\cards\\{}_{}.{}".format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
             card_images.append((10, image,))
+
+
+def deal_card(frame):
+    # next card
+    next_card = deck.pop()
+    # image to Label
+    tkinter.Label(frame, image=next_card[1], relief="raised").pack(side="left")
+    return next_card
+
+
+def deal_dealer():
+    deal_card(dealer_card_frame)
+
+
+def deal_player():
+    deal_card(player_card_frame)
+
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, relief="sunken", borderwidth=1, background="green")
@@ -40,7 +57,7 @@ card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
-tkinter.Label(card_frame, textVariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
+tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
 
 # Frame to hold card images
 dealer_card_frame = tkinter.Label(card_frame, background="green")
@@ -48,19 +65,19 @@ dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Gracz", background="green", fg="white").grid(row=2, column=0)
-tkinter.Label(card_frame, textVariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
 
 # Frame to hold card images
-player_score_frame = tkinter.Frame(card_frame, background="green")
-player_score_label.grid(row=2, column=1, columnspan="ew", rowspan=2)
+player_card_frame = tkinter.Frame(card_frame, background="green")
+player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
 
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky="w")
 
-dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text="Gracz")
+player_button = tkinter.Button(button_frame, text="Gracz", command=deal_player)
 player_button.grid(row=0, column=1)
 
 # load card_images
