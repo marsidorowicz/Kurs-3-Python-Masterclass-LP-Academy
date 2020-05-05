b2c4079679cb2cd3e3c61305414bbc99d223e48a
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-05-05 13:41:18 +0200

    154 finished

--- 151. Black Jack lesson.py
+++ 151. Black Jack lesson.py
@@ -1,44 +1,49 @@
 import random
-import tkinter
+
+try:
+    import tkinter
+except ImportError:  # python 2
+    import Tkinter as tkinter
 
 
 def load_images(card_images):
-    suits = ["heart", "club", "diamond", "spade"]
-    face_cards = ["jack", "queen", "king"]
+    suits = ['heart', 'club', 'diamond', 'spade']
+    face_cards = ['jack', 'queen', 'king']
 
     if tkinter.TkVersion >= 8.6:
-        extension = "png"
+        extension = 'png'
     else:
-        extension = "ppm"
+        extension = 'ppm'
 
-    # retrieve image for each suit
+    # for each suit, retrieve the image for the cards
     for suit in suits:
-        # numbered cards
+        # first the number cards 1 to 10
         for card in range(1, 11):
-            name = "E:\\Programowanie\\cards\\{}_{}.{}".format(str(card), suit, extension)
+            name = 'E:/Programowanie/cards/{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
             card_images.append((card, image,))
 
-        # face cards
+        # next the face cards
         for card in face_cards:
-            name = "E:\\Programowanie\\cards\\{}_{}.{}".format(str(card), suit, extension)
+            name = 'E:/Programowanie/cards/{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
             card_images.append((10, image,))
 
 
 def deal_card(frame):
-    # next card
+    # pop the next card off the top of the deck
     next_card = deck.pop(0)
-    # to back of pack
+    # and add it to back of the pack
     deck.append(next_card)
-    # image to Label
-    tkinter.Label(frame, image=next_card[1], relief="raised").pack(side="left")
+    # add the image to a Label and display the label
+    tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
+    # now return the card's face value
     return next_card
 
 
 def score_hand(hand):
-    # Calc all cards value
-    # Only one ace
+    # Calculate the total score of all cards in the list.
+    # Only one ace can have the value 11, and this will be reduce to 1 if the hand would bust.
     score = 0
     ace = False
     for next_card in hand:
@@ -47,7 +52,7 @@ def score_hand(hand):
             ace = True
             card_value = 11
         score += card_value
-        # if busted check if ace present and subtract 10
+        # if we would bust, check if there is an ace and subtract 10
         if score > 21 and ace:
             score -= 10
             ace = False
@@ -63,13 +68,13 @@ def deal_dealer():
 
     player_score = score_hand(player_hand)
     if player_score > 21:
-        result_text.set("Dealer wygrywa!")
+        result_text.set("Dealer wins!")
     elif dealer_score > 21 or dealer_score < player_score:
-        result_text.set("Gracz wygrywa!")
+        result_text.set("Player wins!")
     elif dealer_score > player_score:
-        result_text.set("Dealer wygrywa!")
+        result_text.set("Dealer wins!")
     else:
-        result_text.set("Remis!")
+        result_text.set("Draw!")
 
 
 def deal_player():
@@ -78,25 +83,25 @@ def deal_player():
 
     player_score_label.set(player_score)
     if player_score > 21:
-        result_text.set("Dealer wygrywa!")
+        result_text.set("Dealer Wins!")
 
 
-def nowa_gra():
+def new_game():
     global dealer_card_frame
     global player_card_frame
     global dealer_hand
     global player_hand
-    # Frame to hold cards
+    # embedded frame to hold the card images
     dealer_card_frame.destroy()
-    dealer_card_frame = tkinter.Frame(card_frame, background="green")
-    dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
-    # Frame to hold cards
-    player_card_frame.destroy()
+    dealer_card_frame = tkinter.Frame(card_frame, background='green')
+    dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
+    # embedded frame to hold the card images
     player_card_frame = tkinter.Frame(card_frame, background="green")
-    player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
+    player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
 
     result_text.set("")
-    # 2 lists player and dealer cards
+
+    # Create the list to store the dealer's and player's hands
     dealer_hand = []
     player_hand = []
 
@@ -109,65 +114,63 @@ def nowa_gra():
 def shuffle():
     random.shuffle(deck)
 
-
 mainWindow = tkinter.Tk()
 
-# Screen setup
-mainWindow.title("Gra Black Jack")
+# Set up the screen and frames for the dealer and player
+mainWindow.title("Black Jack")
 mainWindow.geometry("640x480")
-mainWindow.configure(background="green")
+mainWindow.configure(background='green')
 
-# screen setting
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
 result.grid(row=0, column=0, columnspan=3)
 
 card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
-card_frame.grid(row=1, column=0, sticky="ew", columnspan=3, rowspan=2)
+card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
-tkinter.Label(card_frame, text="Dealer", background="green", fg="white").grid(row=0, column=0)
+tkinter.Label(card_frame, text="Dealer", background="green", fg='white').grid(row=0, column=0)
 tkinter.Label(card_frame, textvariable=dealer_score_label, background="green", fg="white").grid(row=1, column=0)
-
-# Frame to hold card images
-dealer_card_frame = tkinter.Label(card_frame, background="green")
+# embedded frame to hold the card images
+dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
 player_score_label = tkinter.IntVar()
 
-tkinter.Label(card_frame, text="Gracz", background="green", fg="white").grid(row=2, column=0)
+tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=3, column=0)
-
-# Frame to hold card images
+# embedded frame to hold the card images
 player_card_frame = tkinter.Frame(card_frame, background="green")
-player_card_frame.grid(row=2, column=1, sticky="ew", rowspan=2)
+player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
 
 button_frame = tkinter.Frame(mainWindow)
-button_frame.grid(row=3, column=0, columnspan=3, sticky="w")
+button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
 dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text="Gracz", command=deal_player)
+player_button = tkinter.Button(button_frame, text="Player", command=deal_player)
 player_button.grid(row=0, column=1)
 
-new_game_button = tkinter.Button(button_frame, text="Nowa gra", command=nowa_gra)
+new_game_button = tkinter.Button(button_frame, text="New Game", command=new_game)
 new_game_button.grid(row=0, column=2)
 
-shuffle_button = tkinter.Button(button_frame, text="Rozdaj", command=nowa_gra)
+shuffle_button = tkinter.Button(button_frame, text="Shuffle", command=shuffle)
 shuffle_button.grid(row=0, column=3)
 
-# load card_images
+# load cards
 cards = []
 load_images(cards)
-
-# create a new deck of cards and shuffle them
-deck = list(cards)
+print(cards)
+# Create a new deck of cards and shuffle them
+deck = list(cards) + list(cards) + list(cards)
 shuffle()
-# 2 lists player and dealer cards
+
+# Create the list to store the dealer's and player's hands
 dealer_hand = []
 player_hand = []
 
-nowa_gra()
+new_game()
+
 
 mainWindow.mainloop()
