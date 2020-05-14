eedb205295ac7d16a8ea4c56b8e008b4e96fabd4
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-05-14 13:20:05 +0200

    177 finished

---
+++ main.py
@@ -0,0 +1,23 @@
+from player import Player
+
+Mariusz = Player("Mariusz")
+
+
+print(Mariusz.name)
+print(Mariusz)
+Mariusz.lives -= 1
+print(Mariusz)
+Mariusz.lives -= 1
+print(Mariusz)
+Mariusz.lives -= 1
+print(Mariusz)
+Mariusz.lives -= 1
+print(Mariusz)
+Mariusz._lives = 9
+print(Mariusz)
+Mariusz.level = 2
+print(Mariusz)
+Mariusz.level += 5
+print(Mariusz)
+Mariusz.level = 3
+print(Mariusz)
