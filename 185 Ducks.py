d67f69d003fd4c34a679d69e0612533fc34dfdc5
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-05-16 13:33:47 +0200

    186 finished

--- 185 Ducks.py
+++ 185 Ducks.py
@@ -1,4 +1,21 @@
+class Wing(object):
+
+    def __init__(self, ratio):
+        self.ratio = ratio
+
+    def fly(self):
+        if self.ratio > 1:
+            print("Ale zabawa!")
+        elif self.ratio == 1:
+            print("Ciężko ale latam")
+        else:
+            print("Chyba sobie pochodzę")
+
+
 class Duck(object):
+
+    def __init__(self):
+        self._wing = Wing(1.8)
 
     def walk(self):
         print("Klap, klap, klap")
@@ -8,6 +25,9 @@ class Duck(object):
 
     def quack(self):
         print("Kwa, kwa, kwa")
+
+    def fly(self):
+        self._wing.fly()
 
 
 class Penguin(object):
@@ -22,14 +42,15 @@ class Penguin(object):
         print("Jestem pingwin i nie kwaczę")
 
 
-def test_duck(duck):
-    duck.walk()
-    duck.swim()
-    duck.quack()
+# def test_duck(duck):
+#     duck.walk()
+#     duck.swim()
+#     duck.quack()
 
 if __name__ == "__main__":
     donald = Duck()
-    test_duck(donald)
+    donald.fly()
+    # test_duck(donald)
 
-    szeregowy = Penguin()
-    test_duck(szeregowy)
+    # szeregowy = Penguin()
+    # test_duck(szeregowy)
