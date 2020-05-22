252cd3547c45a730f642055c8e68644bfa0e045f
Mariusz Sidorowicz <marsidorowicz@gmail.com>
2020-05-22 18:59:26 +0200

    205 finished

--- 203 challenge contacts.py
+++ 203 challenge contacts.py
@@ -1,6 +1,26 @@
 import sqlite3
 
-db = sqlite3.connect("contacts.sqlite")
+db = sqlite3.connect("contacts.sqlite")  # łączy z istniejącą bazą danych
 
-for row in db.execute("SELECT * FROM contacts"):
-    print(row)
+# for row in db.execute("SELECT * FROM contacts"):
+#     print(row)
+
+name = input("Podaj swoje Imię: ")
+# name = name.capitalize()
+
+# data_for_name = db.execute("SELECT * FROM contacts WHERE name = '{}'".format(name))
+# for name, phone, email in data_for_name:
+#     print(name, phone, email)
+
+# inne podejście z użyciem zapytania
+# data_for_name = db.execute("SELECT * FROM contacts WHERE name = ?", (name,))
+# for name, phone, email in data_for_name:
+#     print(name, phone, email)
+
+# inne podejście by nie używać capitalize
+data_for_name = db.execute("SELECT * FROM contacts WHERE name LIKE ?", (name,))  # po name dajemy przecinek,
+# by wysłać jako tuple
+for name, phone, email in data_for_name:
+    print(name, phone, email)
+
+db.close()
--- 203 contacts sqlite3.py
+++ 203 contacts sqlite3.py
@@ -5,9 +5,14 @@ db = sqlite3.connect("contacts.sqlite")
 # for row in db.execute("SELECT * FROM contacts"):
 #     print(row)
 
-update_sql = "UPDATE contacts SET email = 'mar@pl.pl'"  # WHERE contacts.phone = 4613515"
+new_email = "abc2@even.pl"
+phone = input("Please enter your phone number: ")
+
+# update_sql = "UPDATE contacts SET email = '{}' WHERE contacts.phone = {}".format(new_email, phone)
+update_sql = "UPDATE contacts SET email = ? WHERE phone = ?"  # to avoid sql injection attacks
+print(update_sql)
 update_cursor = db.cursor()
-update_cursor.execute(update_sql)
+update_cursor.execute(update_sql, (new_email, phone))
 print("{} rows updated".format(update_cursor.rowcount))
 
 update_cursor.connection.commit()  # connection to commit instead of commit directly db.commit()
